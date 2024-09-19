6126001b8dcdb95d9c946580bc5c76e5230901a6
DragonPro <76177048+pavlysha123@users.noreply.github.com>
2024-09-19 18:17:12 +0500

    Add files via upload

---
+++ pages/inventory_page.py
@@ -0,0 +1,16 @@
+from playwright.sync_api import Page
+
+
+class InventoryPage:
+    URL = "https://www.saucedemo.com/inventory.html/"
+    ALL_BUTTON_ADDTOCART = "[class='btn btn_primary btn_small btn_inventory ']"
+
+    def __init__(self, page: Page):
+        self.page = page
+        self.load_page()
+
+    def load_page(self):
+        self.page.goto(self.URL)
+
+    def add_item_to_cart(self):
+        self.page.locator(self.ALL_BUTTON_ADDTOCART).all()[0].click()
